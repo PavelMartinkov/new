c4f196889413dfe3d491af4893ff71a9fc0e60f4
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-02-16 15:14:11 +0300

    origin new

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int volume = 1200; // Объем басейна
+        int fillingSpeed = 30; // Литров в минуту
+        int devastationSpeed = 10; // Литров в минуту
+        int startVolume = 0;
+        int minute = 0;
+
+        while (true) {
+            System.out.println(startVolume);
+            startVolume = startVolume + fillingSpeed - devastationSpeed;
+            for (; minute < 60; minute = minute + 2) {
+                continue;
+            }
+            if (startVolume > volume) {
+                System.out.println("Скорость наполнения бассейна: " + minute + " минут");
+                break;
+            }
+        }
+    }
+}
