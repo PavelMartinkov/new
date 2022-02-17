5dfe28d496d0c65489ad28a77d47e23f5a781f9c
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-02-17 18:55:23 +0300

    987

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int volume = 1200; // Объем басейна
 <<<<<<< HEAD
 <<<<<<< Updated upstream:src/Main.java
-        int fillingSpeed = 30; // Литров в минуту
+        int fillingSpeed = 6; // Литров в минуту
         int devastationSpeed = 10; // Литров в минуту
         int startVolume = 3;
         int dlcsd;
