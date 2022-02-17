8be5082e5d066feb9ea04b331a9111bf128743df
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-02-17 18:18:09 +0300

    943r329e21

--- SwimmingPool/src/Main.java
+++ SwimmingPool/src/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main(String[] args) {
         int volume = 1200; // Объем басейна
         int fillingSpeed = 30; // Литров в минут
+<<<<<<< Updated upstream
 int sss;
 int jgt;
+=======
+int privet;
+>>>>>>> Stashed changes
         while (true) {
             System.out.println(startVolume);
             startVolume = startVolume + fillingSpeed - devastationSpeed;
