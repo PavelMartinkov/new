5a1ca243b03029d0297124b67fee612dd043e364
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-02-17 18:02:05 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int volume = 1200; // Объем басейна
-        int fillingSpeed = 40; // Литров в минуту
+        int volume = 900; // Объем басейна
+        int fillingSpeed = 30; // Литров в минуту
         int devastationSpeed = 10; // Литров в минуту
         int startVolume = 0;
-        int startVolume = 10;
-        int startVolume = 0;
-        int minute = 10;
+        int minute = 0;
 
         while (true) {
             System.out.println(startVolume);
